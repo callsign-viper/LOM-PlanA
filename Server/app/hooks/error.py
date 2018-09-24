5c49e714280b382300b28d15ab000609ce00991e
JoMingyu <city7310@naver.com>
2018-09-24 13:50:09 +0900

    [UPDATE] hooks, blueprint 추가, application factory 추가

---
+++ Server/app/hooks/error.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+
+
+def http_exception_handler(e):
+    return jsonify({
+        'result': e.description,
+        'hint': e.description
+    }), e.code
+
+
+def broad_exception_error_handler(e):
+    return jsonify({
+        'result': 'Internal Server Error',
+        'msg': str(e),
+    }), 500
