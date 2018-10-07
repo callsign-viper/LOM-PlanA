90bd42eadec5ef4f97a03f99bc33314e9e86ac81
JoMingyu <city7310@naver.com>
2018-10-07 17:07:51 +0900

    [UPDATE] user agent가 없는 양아치들은 거름

--- Server/app/hooks/request.py
+++ Server/app/hooks/request.py
@@ -1,7 +1,10 @@
-from flask import g, request
+from flask import abort, g, request
 
 
 def before_request():
+    if 'user_agent' not in request.headers:
+        abort(406, 'bring user agent')
+
     g.user_agent = request.headers['user_agent']
     g.remote_addr = request.remote_addr
 
