4509a48cd924e8f194cfb491a1848094c429f87b
JoMingyu <city7310@naver.com>
2018-10-07 17:58:58 +0900

    [UPDATE] flask-jwt-extended의 user loader callback 추가(NotImplemented)

--- Server/app/extensions.py
+++ Server/app/extensions.py
@@ -9,3 +9,15 @@ jwt = JWTManager()
 mongoengine = MongoEngine()
 validator = Validator()
 swagger = Swagger()
+
+
+# @jwt.user_loader_callback_loader
+# def user_loader_callback(identity):
+#     access_token = AccessTokenModel.objects(identity=UUID(identity)).first()
+#     refresh_token = RefreshTokenModel.objects(identity=UUID(identity)).first()
+#
+#     if access_token:
+#         return access_token.key.owner
+#     elif refresh_token:
+#         return refresh_token.key.owner
+# -> current_user가 LocalProxy로 감싸져서, 다루기 힘듬
