6775c30659b4a8132196cd489d4b790c10a4df7f
JoMingyu <city7310@naver.com>
2018-10-09 19:00:42 +0900

    [UPDATE] after_request에서 response payload에 'at'이 없는 경우의 대비

--- Server/app/hooks/request.py
+++ Server/app/hooks/request.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import json
+
 from flask import abort, g, request
 
 
@@ -13,5 +16,21 @@ def after_request(response):
     try:
         response.headers['X-Content-Type-Options'] = 'nosniff'
         response.headers['X-Frame-Options'] = 'deny'
+
+        if response.is_json:
+            response_payload = response.json
+            now_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            if response_payload is None:
+                response.data = json.dumps({
+                    'at': now_str
+                })
+            else:
+                response_payload.update({
+                    'at': now_str
+                })
+                # for 'upsert' dictionary item
+                response.data = json.dumps(response_payload)
+
     finally:
         return response
