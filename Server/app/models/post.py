f96b0214fc33369e179ac699b2167d76a8dee518
JoMingyu <city7310@naver.com>
2018-10-09 17:43:23 +0900

    [DONE] PostModel.post helper classmethod

--- Server/app/models/post.py
+++ Server/app/models/post.py
@@ -21,3 +21,17 @@ class PostModel(Base):
         max_length=3000
     )
 
+    @classmethod
+    def post(cls, user: UserModel, content: str):
+        """
+        Creates new post.
+
+        Args:
+            user: Instance of UserModel. The context in which this method is called will typically be in the g object.
+            content: Content of post. length must between {} and {}.
+        """.format(cls.content.min_length, cls.content.max_length)
+
+        return cls(
+            owner=user,
+            content=content
+        ).save()
