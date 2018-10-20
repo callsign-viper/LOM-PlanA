b561116ec4e385ed2a7872c75b14ab39e80ef6d8
JoMingyu <city7310@naver.com>
2018-10-21 00:00:09 +0900

    [ADD] mongoengine.ValidationError hook handler

--- Server/app/hooks/error.py
+++ Server/app/hooks/error.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from werkzeug.exceptions import BadRequest
 
 
 def http_exception_handler(e):
@@ -6,6 +7,13 @@ def http_exception_handler(e):
         'result': e.name,
         'hint': e.description
     }), e.code
+
+
+def mongoengine_validation_error_handler(e):
+    return jsonify({
+        'result': BadRequest.description,
+        'hint': 'Validation failed - ' + ', '.join(e.to_dict().values())
+    })
 
 
 def broad_exception_error_handler(e):
